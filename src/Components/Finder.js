2a6f89196a56fa5facd301c17df09f53e92785c8
mattcbodily <mattcbodily@gmail.com>
2020-06-24 15:10:15 -0600

    boiler-plate setup

---
+++ src/Components/Finder.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Finder extends Component {
+    render(){
+        return (
+            <div>Finder</div>
+        )
+    }
+}
+
+export default Finder;
---
+++ src/Components/Grass.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Grass extends Component {
+    render(){
+        return (
+            <div>Grass</div>
+        )
+    }
+}
+
+export default Grass;
